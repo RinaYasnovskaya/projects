595eba1b611e3907358b25723886854f835bbb79
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-04-13 22:06:18 +0400

    feat: add burger function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('hello');
+import { cards } from './js/cards'
+
+const items = {
+  elements: {
+    burgerList: [],
+  },
+  properties: {
+    checkBurger: true,
+  },
+}
+
+const init = () => {
+  items.elements.burgerList = document.querySelectorAll('.menu__item');
+}
+// Burger-menu 
+document.querySelector('.burger__button').addEventListener('click', () => {
+  document.querySelector('#burger__toggle').checked = items.checkBurger;
+  items.checkBurger = !items.checkBurger;
+});
+
+
+
+
+window.onload = () => {
+  init();
+}
