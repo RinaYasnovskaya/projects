5b6d012d4420fb822d596925cf855cac91fd0a75
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-04-23 20:05:24 +0400

    chore: edit generate buttons with using document fragment

--- english-for-kids/src/js/generateButtons.js
+++ english-for-kids/src/js/generateButtons.js
@@ -1,16 +1,19 @@
 export const generateButtons = () => {
 
   let sortBlock = document.createElement('div');
-  sortBlock.className = 'sortBlock';
+  sortBlock.className = 'sort-block';
 
-  const nameButtons = ['sort-alphabet', 'sort-train', 'sort-error', 'sort-correct', 'sort-percent', 'reset'];
+  const nameButtons = ['sort-alphabet', 'sort-train', 'sort-error', 'sort-correct', 
+                        'sort-percent', 'reset', 'repeat difficult words'];
+  let fragment = document.createDocumentFragment();
 
   nameButtons.forEach((elem) => {
     let button = document.createElement('button');
     button.id = elem;
     button.textContent = elem;
-    sortBlock.append(button);
+    fragment.append(button);
   });
 
+  sortBlock.append(fragment);
   document.getElementById('innerVocabulary').append(sortBlock);
 }
