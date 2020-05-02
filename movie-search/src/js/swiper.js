64df59ce68ed3901279f34ba2cfdef240fa57b30
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-05-02 19:38:57 +0400

    feat: add func createCard & change html structure for that

--- movie-search/src/js/swiper.js
+++ movie-search/src/js/swiper.js
@@ -1,11 +1,31 @@
 import '../../node_modules/swiper/css/swiper.min.css';
 import Swiper from '../../node_modules/swiper/js/swiper';
 
+// apikey = 23d60cc6
+
 async function createCard(title) {
-  const response = await fetch(`https://www.omdbapi.com/?apikey=b74de48f&s=${title}`);
+  const response = await fetch(`http://www.omdbapi.com/?apikey=23d60cc6&s=${title}`);
   const resArray = await response.json();
 
-  console.log(resArray);
+  let resTemplate = '';
+
+  for (const item of resArray.Search) {
+    const responseIMD = await fetch(`https://www.omdbapi.com/?apikey=23d60cc6&i=${item.imdbID}`);
+    const resIMD = await responseIMD.json();
+
+    const template = `<div class="swiper-slide slide">
+      <a href="" class="slide__header">${item.Title}</a>
+      <div class="poster" style="background-image: url(${item.Poster})"></div>
+      <div class="year">${item.Year}</div>
+      <div class="imbscore">
+      <span class="imbscore__star"><img src="./src/img/star.svg" alt=""></span>${resIMD.imdbRating}</div>
+    </div>`;
+
+    resTemplate += template;
+  }
+  
+  document.querySelector('.swiper-wrapper').innerHTML = resTemplate;
+  startSwiper();
 }
 
 
@@ -26,6 +46,6 @@ const startSwiper = () => {
 };
 
 window.onload = () => {
-  startSwiper();
+  // startSwiper();
   createCard('dream');
 };
