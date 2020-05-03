2ce0866da4d64193a505821a81563ef9888b3f9d
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-05-03 18:33:52 +0400

    feat: add path existence check

--- movie-search/src/js/renderCards.js
+++ movie-search/src/js/renderCards.js
@@ -9,13 +9,15 @@ export async function renderCards(title) {
     const resIMD = await responseIMD.json();
 
     const poster = (item.Poster !== 'N/A') ? item.Poster : './src/img/none.png';
+    const year = (item.Year !== 'N/A') ? item.Year : '????';
+    const rating = (resIMD.imdbRating !== 'N/A') ? resIMD.imdbRating : '?.?';
 
     const template = `<div class="swiper-slide slide">
       <a href="https://www.imdb.com/title/${item.imdbID}/videogallery" class="slide__header">${item.Title}</a>
       <div class="poster" style="background-image: url(${poster})"></div>
-      <div class="year">${item.Year}</div>
+      <div class="year">${year}</div>
       <div class="imbscore">
-      <span class="imbscore__star"><img src="./src/img/star.svg" alt=""></span>${resIMD.imdbRating}</div>
+      <span class="imbscore__star"><img src="./src/img/star.svg" alt=""></span>${rating}</div>
       </div>`;
     resTemplate += template;
   }
