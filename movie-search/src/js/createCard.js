fcbded50cda02ce0b37b04940b83e9377d884ea5
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-05-10 16:01:25 +0400

    chore: rewrite http to https

--- movie-search/src/js/createCard.js
+++ movie-search/src/js/createCard.js
@@ -3,7 +3,7 @@ import { startSwiper } from './startSwiper';
 import { title, page, apikey } from './swiper';
 
 export const createCard = async() => {
-  const response = await fetch(`http://www.omdbapi.com/?apikey=${apikey}&page=${page}&s=${title}`);
+  const response = await fetch(`https://www.omdbapi.com/?apikey=${apikey}&page=${page}&s=${title}`);
   const resArray = await response.json();
   const resultTemplate = await renderCards(resArray);
 
--- movie-search/src/js/giveMovieResult.js
+++ movie-search/src/js/giveMovieResult.js
@@ -1,7 +1,7 @@
 import { page, title, apikey } from './swiper';
 
 export const giveMovieResult = async () => {
-  const response = await fetch(`http://www.omdbapi.com/?apikey=${apikey}&page=${page}&s=${title}`);
+  const response = await fetch(`https://www.omdbapi.com/?apikey=${apikey}&page=${page}&s=${title}`);
   const responseArray = await response.json();
   return responseArray;
 };
