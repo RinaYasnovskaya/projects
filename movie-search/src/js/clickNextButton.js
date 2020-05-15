c2bdc03d1173ded55ed5d90542a01b3c96bf92e2
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-05-15 17:09:08 +0400

    chore: delete incorrect code

--- movie-search/src/js/clickNextButton.js
+++ movie-search/src/js/clickNextButton.js
@@ -1,7 +1,7 @@
 import { prevSlides } from './prevSlides';
 import { result, innerActions } from './swiper';
 
-document.querySelector('.swiper-button-next').addEventListener('click', async () => {
+document.querySelector('.swiper-button-next').addEventListener('click', () => {
   result.innerText = '';
   const slider = document.querySelector('.swiper-container').swiper;
   innerActions(slider);
