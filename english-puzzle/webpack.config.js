b27ae867331e4687712bdef4513774db5c390c52
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-06-03 18:35:41 +0400

    init: start task english puzzle & add basic structure

---
+++ english-puzzle/webpack.config.js
@@ -0,0 +1,167 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const ENV = process.env.npm_lifecycle_event;
+const isDev = ENV === 'dev';
+const isProd = ENV === 'prod';
+
+function setDevTool() {
+  if (isDev) {
+    return 'cheap-module-eval-source-map';
+  }
+  return 'none';
+}
+
+function setDMode() {
+  if (isProd) {
+    return 'production';
+  }
+  return 'development';
+  
+}
+
+const config = {
+  target: "web",
+  entry: ['./src/index.js', './src/sass/style.scss'],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'script.js'
+  },
+  mode: setDMode(),
+  devtool: setDevTool(),
+  module: {
+    rules: [{
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: false
+          }
+        }]
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: './postcss.config.js' } }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: './postcss.config.js' } }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: './src/img',
+              name: '[name].[ext]'
+            }},
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug : true,
+              mozjpeg: {
+                progressive: true,
+                quality: 75
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+                optimizationLevel: 1
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            outputPath: 'fonts'
+          }
+        }]
+      },
+    ]
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: './src/sass/style.css',
+    }),
+    new CopyWebpackPlugin([
+      {from: './src/img', to: './src/img/'},
+    ]),
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, '/dist'),
+    compress: true,
+    port: 3000,
+    overlay: true,
+    stats: 'errors-only',
+    clientLogLevel: 'none'
+  }
+}
+
+if (isProd) {
+  config.plugins.push(
+    new UglifyJSPlugin(),
+  );
+};
+
+module.exports = config;
