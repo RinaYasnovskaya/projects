d591f6e9d9e879cf2dd5525948c61cebb81411ee
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-06-13 17:01:45 +0400

    chore: add check to correct fetch response

--- english-puzzle/src/js/createUser.js
+++ english-puzzle/src/js/createUser.js
@@ -1,16 +1,26 @@
 import { saveUser } from "./saveUser";
 
+const correctStatus = 200;
+
 export const createUser = async (user, count, globalUser) => {
-  const response = await fetch('https://afternoon-falls-25894.herokuapp.com/users', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(user)
-  });
-  const content = await response.json();
-  globalUser[count].id = content.id;
-  console.log(content, globalUser)
-  saveUser();
+  try{
+    const response = await fetch('https://afternoon-falls-25894.herokuapp.com/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+    if (response.Response && response.Response.status !== correctStatus) {
+      throw new Error;
+    }
+    const content = await response.json();
+    globalUser[count].id = content.id;
+    saveUser();
+  } 
+  catch (err) {
+    alert('Application error. Try later');
+  }
+  
 };
--- english-puzzle/src/js/loginUser.js
+++ english-puzzle/src/js/loginUser.js
@@ -1,19 +1,27 @@
 import { saveUser } from './saveUser';
 
 export const loginUser = async (user, globalUser) => {
-  const rawResponse = await fetch('https://afternoon-falls-25894.herokuapp.com/signin', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(user)
-  });
-  const content = await rawResponse.json();
-  globalUser.forEach((elem) => {
-    if (elem.email === user.email) {
-      elem.token = content.token;
+  try {
+    const rawResponse = await fetch('https://afternoon-falls-25894.herokuapp.com/signin', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+    if (response.Response && response.Response.status !== correctStatus) {
+      throw new Error;
     }
-  });
-  saveUser();
+    const content = await rawResponse.json();
+    globalUser.forEach((elem) => {
+      if (elem.email === user.email) {
+        elem.token = content.token;
+      }
+    });
+    saveUser();
+  }
+  catch (err) {
+    alert('Application error. Try later');
+  }
 };
